a6939ac9af28f164f7ca2f760918c58fe6a86d86
Josh Milbrandt <jdmsolarius@gmail.com>
2023-01-02 14:42:03 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using LazyCache;
+using LazyCache.Providers;
+using LibAlignerMiniature.Models.DB;
+using Microsoft.Extensions.Caching.Memory;
+
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+builder.Services.AddMemoryCache();
+builder.Services.AddDbContext<AzazeldbContext>(ServiceLifetime.Transient);
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+    
+app.Run();
